8347d9bc857b12e0a904bcaf968bf1ffebebafca
ab-ha-y <abhay.kx@gmail.com>
2021-02-28 12:25:27 +0530

    variablizing keyvault name in remediation script

--- remediate.bash
+++ remediate.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-az keyvault create --name ab-key-vault-vmcrypt --resource-group rg-ind-abhay --location centralindia --enable-soft-delete true --enable-purge-protection true --enabled-for-disk-encryption true
+echo "Creating Key Vault..."
+KEYVAULT=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
+echo "${KEYVAULT}"
+az keyvault create --name $KEYVAULT --resource-group rg-ind-abhay --location centralindia --enable-soft-delete true --enable-purge-protection true --enabled-for-disk-encryption true
 
 echo "Encrypting Data Disk..."
 while IFS= read -r line
 do
-   cmd="az vm encryption enable --ids ${line} --disk-encryption-keyvault ab-key-vault-vmcrypt --volume-type DATA"
+   cmd="az vm encryption enable --ids ${line} --disk-encryption-keyvault $KEYVAULT --volume-type DATA"
    eval ${cmd}
 done < vmlist.txt
 echo "Data Disk Encryption Completed."
