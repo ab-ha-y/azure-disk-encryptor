7856dee1e99f1f0ac508bd72503902516207146d
ab-ha-y <abhay.kx@gmail.com>
2021-02-28 13:05:26 +0530

    Final Commit

--- remediate.bash
+++ remediate.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+RG=rg-ind-xxxxx #Replace the resource group name to a valid one
+LOC=centralindia #Change Azure location
+
 echo "Creating Key Vault..."
 KEYVAULT=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
 echo "${KEYVAULT}"
-az keyvault create --name $KEYVAULT --resource-group rg-ind-abhay --location centralindia --enable-soft-delete true --enable-purge-protection true --enabled-for-disk-encryption true
+az keyvault create --name $KEYVAULT --resource-group $RG --location $LOC --enable-soft-delete true --enable-purge-protection true --enabled-for-disk-encryption true
 
 echo "Encrypting Data Disk..."
 while IFS= read -r line
