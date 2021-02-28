1a581af635c28612476cdd763a86ca7313129ef6
ab-ha-y <abhay.kx@gmail.com>
2021-02-28 12:15:47 +0530

    adding audit script

---
+++ audit.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+exec 2> ./log.txt
+
+#Login to Azure CLI
+az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
+
+#List all VM
+az vm list --query '[*].id' > vmlist.txt
+
+
+# Check if the data disk are encrypted
+(
+while IFS= read -r line
+do
+    echo ${line}
+    cmd="az vm encryption show --ids ${line} --output table"
+    eval ${cmd}
+    echo -e "\n"
+done < vmlist.txt
+)>audit.txt
+
